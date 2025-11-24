cdc6c112685d95abfcbaab1c555fde6255d0d851
janatha8738 <janathagaveshana@gmail.com>
2025-11-24 10:06:55 +0530

    g

--- .next/dev/static/chunks/app_news_[id]_page_tsx_d57ee374._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/app_news_[id]_page_tsx_668187f3._.js"
-],
-    source: "dynamic"
-});
