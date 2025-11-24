fdbe2dbeed4a2d81d6770ed1c105be3ce674c046
janatha8738 <janathagaveshana@gmail.com>
2025-11-24 11:14:46 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import Hero from "./components/Hero";
-import NewsSection from "@/app/components/NewsSection";
+import Hero from "./Components/Hero";
+import NewsSection from "@/app/Components/NewsSection";
 
 export default function HomePage() {
   return (
